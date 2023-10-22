23ae9190df37f5df1295f96d14b7bc7f8456dbbe
Gdepablo <guille_de_pablo@outlook.com>
2023-10-22 00:28:40 -0300

    test

--- src/main/java/Backend/Security/LoginConfiguration.java
+++ src/main/java/Backend/Security/LoginConfiguration.java
@@ -1,16 +1,10 @@
 package Backend.Security;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import java.util.List;
 
 
 @Configuration
